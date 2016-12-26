e2d095b62d1d87cede45b77c715942bc61fc5214
ondigitalDesarrollo <ondigital.desarrollo@gmail.com>
2016-12-26 12:55:52 -0500

    Agregamos la funcion para la consulta de los datos

--- application.php
+++ application.php
@@ -28,6 +28,34 @@ $server->register('MetodoConsulta',
         'Retorna el datos' 
 );
 
+//Metodo de la consulta
+function MetodoConsulta($param_id,$param_text){
+    //Conectamos y seleccionamos la base de datos
+    $link = mysql_connect(SQL_SERVER,SQL_USER,SQL_USER_PASS) or die("Error: ".mysal_error());
+    $ddbb = mysql_select_db(SQL_DB)or die("Error ".mysal_error());
+    
+    //Realizar la consulta Mysql
+    $query = "SELECT * FROM articulos WHERE id = '$param_id' AND txt = '$param_text'";
+    $result = mysql_query($query) or die('Consulta Fallida' . mysql_error());
+    
+    //Tratar datos seleccionados
+    $row = mysql_fetch_array($result);
+    
+    //Obtener los campos seleccionados
+    $descripcion    = $row['txt'];
+    $precio         = $row['precio'];
+    
+    // Devolver el descriptivo y el precio consultado
+    return "RESULTADO =".strtoupper($descripcion)."\n". strtoupper($precio)."$";
+    
+    // liberar resultados
+    mysql_free_result($result);
+    
+    // Cerrar la conexion
+    mysql_close($link);
+            
+}
+
 
 
 
